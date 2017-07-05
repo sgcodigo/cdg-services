2e8458c2e7fd8d74ac9a8d0f9dab54e1fb3fabc7
Vic-L <vljc17@gmail.com>
2017-07-06 00:22:34 +0800

    feat: improve services

--- lib/cdg/services.rb
+++ lib/cdg/services.rb
@@ -1,7 +1,41 @@
 require "cdg/services/version"
+require "net/http"
+require "json"
+require "pry"
+require "nokogiri"
+require "open-uri"
 
 module CDG
+
   module Services
-    # Your code goes here...
+
+    def self.GetIOSVersion app_id: # eg 123456789
+      resp = JSON.parse Net::HTTP.get(URI("https://itunes.apple.com/lookup?id=#{app_id}"))
+      results = resp["results"]
+
+      if results.empty?
+        nil
+      else
+        results[0]["version"]
+      end
+    end
+
+    def self.GetAndroidVersion app_id: # eg sg.codigo.app_name
+      begin
+        resp = Nokogiri::HTML(open("https://play.google.com/store/apps/details?id=#{ app_id }&hl=en"))
+        elements = resp.css("div[class='content'][itemprop='softwareVersion']")
+
+        if elements.empty?
+          nil
+        else
+          elements[0].text.strip
+        end
+      rescue OpenURI::HTTPError => e
+        return nil
+      end
+
+    end
+
   end
+
 end
--- spec/cdg/services_spec.rb
+++ spec/cdg/services_spec.rb
@@ -1,11 +1,37 @@
 require "spec_helper"
 
 RSpec.describe CDG::Services do
+
   it "has a version number" do
     expect(CDG::Services::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe "#GetIOSVersion" do
+
+    it "should return nil if invalid iOS app id is given" do
+      expect(CDG::Services.GetIOSVersion(app_id: 123)).to eq(nil)
+    end
+
+    it "should return the version number in String if a valid iOS app id is given" do
+      app_id = "284882215" # facebook app id on App Store
+      app_version = CDG::Services.GetIOSVersion(app_id: app_id)
+      expect(app_version).not_to eq(nil)
+    end
+
   end
+
+  describe "#GetAndroidVersion" do
+
+    it "should return nil if invalid android app id is given" do
+      expect(CDG::Services.GetAndroidVersion(app_id: 123)).to eq(nil)
+    end
+
+    it "should return the version number in String if a valid android app id is given" do
+      app_id = "com.facebook.katana" # facebook app id on Play Store
+      app_version = CDG::Services.GetAndroidVersion(app_id: app_id)
+      expect(app_version).not_to eq(nil)
+    end
+
+  end
+
 end
