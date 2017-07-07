3b6208268ac4e860955607b0491608a332e2d7c2
Vic-L <vljc17@gmail.com>
2017-07-07 11:57:16 +0800

    test: changed method naming following conventions

--- spec/cdg/services_spec.rb
+++ spec/cdg/services_spec.rb
@@ -6,29 +6,29 @@ RSpec.describe CDG::Services do
     expect(CDG::Services::VERSION).not_to be nil
   end
 
-  describe "#GetIOSVersion" do
+  describe "#get_ios_version" do
 
     it "should return nil if invalid iOS app id is given" do
-      expect(CDG::Services.GetIOSVersion(app_id: 123)).to eq(nil)
+      expect(CDG::Services.get_ios_version(app_id: 123)).to eq(nil)
     end
 
     it "should return the version number in String if a valid iOS app id is given" do
       app_id = "284882215" # facebook app id on App Store
-      app_version = CDG::Services.GetIOSVersion(app_id: app_id)
+      app_version = CDG::Services.get_ios_version(app_id: app_id)
       expect(app_version).not_to eq(nil)
     end
 
   end
 
-  describe "#GetAndroidVersion" do
+  describe "#get_android_version" do
 
     it "should return nil if invalid android app id is given" do
-      expect(CDG::Services.GetAndroidVersion(app_id: 123)).to eq(nil)
+      expect(CDG::Services.get_android_version(app_id: 123)).to eq(nil)
     end
 
     it "should return the version number in String if a valid android app id is given" do
       app_id = "com.facebook.katana" # facebook app id on Play Store
-      app_version = CDG::Services.GetAndroidVersion(app_id: app_id)
+      app_version = CDG::Services.get_android_version(app_id: app_id)
       expect(app_version).not_to eq(nil)
     end
 
